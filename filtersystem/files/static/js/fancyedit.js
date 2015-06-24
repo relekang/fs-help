235b055f57fb8ef975207b985df8335e8ba7665b
Rolf Erik Lekang <me@rolflekang.com>
2015-06-24 21:07:15 +0200

    Add static files

---
+++ filtersystem/files/static/js/fancyedit.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $("#id_content").wymeditor({ // "FIELDNAME" is the name of the field you want to give the wysiwyg features
+    updateSelector: "input:submit",
+    updateEvent: "click"
+  });
+});
---
+++ filtersystem/files/static/js/tiny_mce/conf.js
@@ -0,0 +1,17 @@
+$().ready(function() {
+    $('textarea').tinymce({
+        script_url : '/static/js/tiny_mce/tiny_mce.js',
+        content_css : '/static/tiny_mce.css',
+        theme : "advanced",
+        plugins : "spellchecker,preview",
+
+        // Theme options - button# indicated the row# only
+        theme_advanced_buttons1 : "bold,italic,underline,forecolor,|,justifyleft,justifycenter,justifyright,|,sub,sup,formatselect|,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,anchor,image,|,code,preview,",
+        theme_advanced_buttons2 : "",
+        theme_advanced_buttons3 : "",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_resizing : true,
+        theme_advanced_statusbar_location : "bottom",
+        valid_styles : { '*' : 'color,font-weight,font-style,text-decoration' }
+    });
+});
